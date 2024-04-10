e619b1fd80bae87d942a720e2ca5ba0d9aad295d
MikelGonzalezUD <mikel.g.t@opendeusto.es>
2024-04-10 18:00:10 +0200

    Añadir menu

--- main.c
+++ main.c
@@ -4,29 +4,54 @@
 #include "formula.h"
 
 void mostrarMenu(){
+    printf("\n");
+    printf("Elige una de las siguientes opciones\n");
     printf("1. Realiza una suma\n");
+    printf("2. Realiza una resta\n");
+    printf("3. Realiza una multiplicacion\n");
+    printf("Pulsa 'q' para terminar\n");
 }
 
 void main (void){
-    int opcion;
+    char opcion;
     float num1;
     float num2;
 
-    mostrarMenu();
-
-    scanf("%i", &opcion);
-    switch (opcion)
+    do
     {
-    case 1:
-        
-        printf("Introduce el primer numero:\n");
-        scanf("%f", &num1);
-        printf("Introduce el segundo numero:\n");
-        scanf("%f", &num2);
-        printf("El resultado de la suma es %f", sumar(num1, num2));
-        break;
+        mostrarMenu();
+
+        scanf("%c", &opcion);
+        switch (opcion)
+            {
+            case '1':
+                printf("Introduce el primer numero:\n");
+                scanf("%f", &num1);
+                printf("Introduce el segundo numero:\n");
+                scanf("%f", &num2);
+                printf("El resultado de la suma es %.2f\n", sumar(num1, num2));
+                break;
+
+            case '2':
+                printf("Introduce el primer numero:\n");
+                scanf("%f", &num1);
+                printf("Introduce el segundo numero:\n");
+                scanf("%f", &num2);
+                printf("El resultado de la resta es %.2f\n", restar(num1, num2));
+
+            case '3':
+                printf("Introduce el primer numero:\n");
+                scanf("%f", &num1);
+                printf("Introduce el segundo numero:\n");
+                scanf("%f", &num2);
+                printf("El resultado de la multiplicacion es %.2f\n", multiplicar(num1, num2));
+
+
+            default:
+                break;
+        }
+    fflush(stdin);
+    } while (opcion != 'q');
     
-    default:
-        break;
-    }
+    
 }
