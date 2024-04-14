f2e6dc8954b3aeb95b275fb5742e49000964ca77
MikelGonzalezUD <mikel.g.t@opendeusto.es>
2024-04-14 11:15:17 +0200

    Mejora del log, funcion de registro e inicio de sesion

--- main.c
+++ main.c
@@ -5,12 +5,28 @@
 #include "binario.h"
 #include <time.h>
 
+#define MAX_CAR 20
 #define MAX_DIG 10
 #define FIC_LOG "log.txt"
 
+char registroMenu(){
+
+    char opcionRegistro[2];
+    printf("\nInicia sesion o registrate si no tienes una cuenta antes de usar la calculadora\n");
+    printf("Introduce una de las siguientes opciones:\n");
+    printf("1. Registrarse\n");
+    printf("2. Iniciar sesion\n");
+
+    fflush(stdout);
+    fgets(opcionRegistro, 2, stdin);
+
+    return *opcionRegistro;
+}
+
 char mostrarMenu(){
-    char opcion[2];
-    printf("\n");
+    char opcionCalc[2];
+    fflush(stdout);
+    printf("\n--Bienvenido a la calculadora--\n");
     printf("Elige una de las siguientes opciones\n");
     printf("1. Realiza una suma\n");
     printf("2. Realiza una resta\n");
@@ -23,9 +39,9 @@ char mostrarMenu(){
     printf("Pulsa 'q' para terminar\n");
 
     fflush(stdout);
-    fgets(opcion, 2, stdin);
+    fgets(opcionCalc, 2, stdin);
 
-    return *opcion;
+    return *opcionCalc;
 
 }
 
@@ -37,19 +53,29 @@ float obtenerDeTeclado(){
     return num;
 }
 
+void registrarAccion(char *accion, FILE* ficheroLog) {
+
+    time_t tiempoActual = time(NULL);
+    struct tm *local = localtime(&tiempoActual);
+    
+    char formatoTiempo[20];
+    strftime(formatoTiempo, sizeof(formatoTiempo), "%d/%m/%y %H:%M:%S", local);
+    
+    fprintf(ficheroLog, "[%s] %s\n", formatoTiempo, accion);
+}
+
 void main (void){
 
-    FILE* fichero_log = fopen(FIC_LOG, "a");
+    FILE* ficheroLog = fopen(FIC_LOG, "a");
 
-    time_t tiempo_actual = time(NULL);
-    struct tm *local = localtime(&tiempo_actual);
+    registrarAccion("Se ha iniciado el programa", ficheroLog);
 
-    char formato_tiempo[20];
-    strftime(formato_tiempo, sizeof(formato_tiempo), "%d/%m/%y %H:%M:%S", local);
+    char opcionRegistro;
+    int inicioSesionCompletado = 0;
+    char nombre[MAX_CAR];
+    char contrasenya[MAX_CAR];
 
-    fprintf(fichero_log, "[%s] Se ha iniciado el programa\n", formato_tiempo);
-
-    char opcionTeclado;
+    char opcionCalc;
     int numeroATransformarBinario;
     int ordenMatriz; 
     int determinante;
@@ -57,23 +83,54 @@ void main (void){
     float num1;
     float num2;
 
+    while(inicioSesionCompletado == 0){
+        opcionRegistro = registroMenu();   
+
+        if (opcionRegistro == '1'){
+            registrarAccion("Se ha iniciado registro de cuenta", ficheroLog);
+            fflush(stdin);
+            printf("Introduce tu nombre (maximo 20 caracteres):\n");
+            fgets(nombre, MAX_CAR, stdin);
+            printf("Introduce tu contrasenya (maximo 20 caracteres):\n");
+            fgets(contrasenya, MAX_CAR, stdin);
+            /*funcion para agregar a la base de datos*/
+            printf("Registro completado correctamente\n");
+        }else{
+            if (opcionRegistro == '2'){
+                registrarAccion("Se ha iniciado el inicio de sesion", ficheroLog);
+                fflush(stdin);
+                printf("Intrduce to nombre:\n");
+                fgets(nombre, MAX_CAR, stdin);
+                printf("Introduce tu contrasenya:\n");
+                fgets(contrasenya, MAX_CAR, stdin);
+                /*funcion para verficar*/
+                inicioSesionCompletado = 1; /*Si se ha completado correctamente*/
+            }else{
+                fflush(stdin);
+                printf("Haz introducido una opcion no disponible\n");
+                registrarAccion("El usuario ha introducido una opcion de registro no disponible", ficheroLog);
+            }       
+        }    
+    }
+    registrarAccion("El usuario ha completado el resgitro, se inicia la calculadora", ficheroLog);
+
+
     do
     {
-        opcionTeclado = mostrarMenu();
+        opcionCalc = mostrarMenu();
 
 
-        if ((opcionTeclado >= '1' && opcionTeclado <= '9') || opcionTeclado == 'q'){
+        if ((opcionCalc >= '1' && opcionCalc <= '9') || opcionCalc == 'q'){
             /*Al recibir un numero grande como 67 de opcion, la variable al ser un char coge el numero como si fuese 6*/
             fflush(stdin);
-            if (opcionTeclado == 'q'){
-                fprintf(fichero_log, "[%s] Se ha terminado el programa\n", formato_tiempo);
-            }else{
-                fprintf(fichero_log, "[%s] Se ha seleccionado la opcion %c del menu\n", formato_tiempo, opcionTeclado);
+            if (opcionCalc == 'q'){
+                registrarAccion("Se ha terminado el programa", ficheroLog);
             }
             
-            switch (opcionTeclado)
+            switch (opcionCalc)
             {
             case '1':
+                registrarAccion("Se ha seleccionado la opcion 1 del menu", ficheroLog);
                 printf("Introduce el primer numero:\n");
                 num1 = obtenerDeTeclado();
                 printf("Introduce el segundo numero:\n");
@@ -82,6 +139,7 @@ void main (void){
                 break;
 
             case '2':
+                registrarAccion("Se ha seleccionado la opcion 2 del menu", ficheroLog);
                 printf("Introduce el primer numero:\n");
                 num1 = obtenerDeTeclado();
                 printf("Introduce el segundo numero:\n");
@@ -90,6 +148,7 @@ void main (void){
                 break;
 
             case '3':
+                registrarAccion("Se ha seleccionado la opcion 3 del menu", ficheroLog);
                 printf("Introduce el primer numero:\n");
                 num1 = obtenerDeTeclado();
                 printf("Introduce el segundo numero:\n");
@@ -98,6 +157,7 @@ void main (void){
                 break;
             
             case '4':
+                registrarAccion("Se ha seleccionado la opcion 4 del menu", ficheroLog);
                 printf("Introduce el primer numero:\n");
                 num1 = obtenerDeTeclado();
                 printf("Introduce el segundo numero:\n");
@@ -106,6 +166,7 @@ void main (void){
                 break;
 
             case '5':
+                registrarAccion("Se ha seleccionado la opcion 5 del menu", ficheroLog);
                 printf("Introduce la base:\n");
                 num1 = obtenerDeTeclado();
                 printf("Introduce el exponente:\n");
@@ -114,12 +175,14 @@ void main (void){
                 break;
 
             case '6':
+                registrarAccion("Se ha seleccionado la opcion 6 del menu", ficheroLog);
                 printf("Introduce el numero para realizar la raiz cuadrada:\n");
                 num1 = obtenerDeTeclado();
                 printf("El resultado de la raiz es %.2f\n", raizCuadrada(num1));
                 break;
 
             case '7':
+                registrarAccion("Se ha seleccionado la opcion 7 del menu", ficheroLog);
                 printf("Ingresa el orden de la matriz cuadrada: \n");
                 scanf("%d", &ordenMatriz);
 
@@ -151,16 +214,17 @@ void main (void){
                 break;
 
             case '8':
+                registrarAccion("Se ha seleccionado la opcion 8 del menu", ficheroLog);
                 printf("Introduce el numero a transformar:\n");
                 scanf("%i", &numeroATransformarBinario);
                 InfoBinario ib = binarios(numeroATransformarBinario);
 
-                printf("El numero binario de %i es: \n", numeroATransformarBinario);
+                printf("El numero binario de %i es: ", numeroATransformarBinario);
                 for (int i = 0; i < ib.cantidadBits; i++)
                 {
                     printf("%i", ib.numeroBinario[i]);
                 }
-
+                printf("\n");
                 free(ib.numeroBinario);
 
             default:
@@ -168,11 +232,11 @@ void main (void){
         }
         }else{
             printf("Has introducido una opcion no disponible\n");
-            fprintf(fichero_log, "[%s] El usuario ha elegio una funcion no disponible",  formato_tiempo);
+            registrarAccion("El usuario ha elegido una funcion no disponible", ficheroLog);
         }
 
     fflush(stdin);
-    } while (opcionTeclado != 'q');
+    } while (opcionCalc != 'q');
     
-    
+    fclose(ficheroLog);
 }
