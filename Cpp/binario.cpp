280f62c68ddd5aaf22fe2913c9c7a3dda73d795b
Gontzaa86 <144795395+Gontzaa86@users.noreply.github.com>
2024-05-24 15:14:22 +0200

    Bases de C++
    Añadida carpeta con código de transformación de binario a entero y de entero a binario.
    El archivo "main.cpp" contiene un menu interactivo

---
+++ Cpp/binario.cpp
@@ -0,0 +1,28 @@
+#include "binario.h"
+#include <cmath>
+
+std::string transformarBinario(int numero) 
+{
+    if (numero == 0) return "0";
+    std::string binario = "";
+    while (numero > 0) 
+    {
+        binario = (numero % 2 == 0 ? "0" : "1") + binario;
+        numero /= 2;
+    }
+    return binario;
+}
+
+int transformarEntero(const std::string& binario)
+{
+    int numero = 0;
+    int potencia = 0;
+    for (int i = binario.length() - 1; i >= 0; --i) 
+    {
+        if (binario[i] == '1') {
+            numero += std::pow(2, potencia);
+        }
+        ++potencia;
+    }
+    return numero;
+}
